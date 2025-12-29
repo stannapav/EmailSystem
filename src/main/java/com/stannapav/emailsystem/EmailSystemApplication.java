7091c6f0c91d32468accdcdf2e911056dcb40913
stannapav <stannapav@gmail.com>
2025-12-29 23:13:17 +0200

    innit project

---
+++ src/main/java/com/stannapav/emailsystem/EmailSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stannapav.emailsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stannapav/emailsystem/EmailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stannapav.emailsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
