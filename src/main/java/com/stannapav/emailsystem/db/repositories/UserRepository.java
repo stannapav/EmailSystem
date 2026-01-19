605d08ffc0f325a84cb47d6bae1c777134215a51
stannapav <stannapav@gmail.com>
2026-01-19 23:21:35 +0200

    Repository for user made

---
+++ src/main/java/com/stannapav/emailsystem/db/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package com.stannapav.emailsystem.db.repositories;
+
+import com.stannapav.emailsystem.db.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findAllByOrderByCreatedOnDesc();
+
+    Page<User> findAllByOrderByCreatedOnDesc(Pageable pageable);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
