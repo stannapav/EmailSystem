9e7eeede65e8173403ddb1956123f2d7e0abd09d
stannapav <stannapav@gmail.com>
2026-01-19 23:35:22 +0200

    Exception handler made

---
+++ src/main/java/com/stannapav/emailsystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.stannapav.emailsystem.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
