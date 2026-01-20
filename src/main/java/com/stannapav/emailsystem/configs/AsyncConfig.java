1cec25104a85919641a3a4060b49013fa61f99da
stannapav <stannapav@gmail.com>
2026-01-21 01:11:06 +0200

    Adding async to project

---
+++ src/main/java/com/stannapav/emailsystem/configs/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.stannapav.emailsystem.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+    @Bean(name = "mailExecutor")
+    public Executor mailExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Mail-Async-");
+        executor.initialize();
+        return executor;
+    }
+}
