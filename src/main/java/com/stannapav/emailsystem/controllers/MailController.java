1e3eb650e3639a7731d8e8725af025251eb40a81
stannapav <stannapav@gmail.com>
2026-01-21 01:00:27 +0200

    Handling of email sending exceptions

---
+++ src/main/java/com/stannapav/emailsystem/controllers/MailController.java
@@ -0,0 +1,28 @@
+package com.stannapav.emailsystem.controllers;
+
+import com.stannapav.emailsystem.db.entities.User;
+import com.stannapav.emailsystem.db.services.MailService;
+import com.stannapav.emailsystem.db.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/mails")
+public class MailController {
+    private final MailService mailService;
+    private final UserService userService;
+
+    @PostMapping("/send/{userId}")
+    public ResponseEntity<Void> sendMailToUser(@PathVariable Integer userId) {
+        User user = userService.getUserById(userId);
+        mailService.sendUserMail(user);
+
+        return ResponseEntity.status(200).build();
+    }
+}
