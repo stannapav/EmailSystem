afc41ee343c778800ba5db8b2244e661e9e7c8b9
stannapav <stannapav@gmail.com>
2026-01-21 15:10:58 +0200

    configuring cron schedules

---
+++ src/main/java/com/stannapav/emailsystem/validation/CronValidator.java
@@ -0,0 +1,25 @@
+package com.stannapav.emailsystem.validation;
+
+import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinition;
+import com.cronutils.model.definition.CronDefinitionBuilder;
+import com.cronutils.parser.CronParser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CronValidator {
+    private final CronParser cronParser;
+
+    public CronValidator() {
+        CronDefinition definition = CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ);
+        this.cronParser = new CronParser(definition);
+    }
+
+    public void validate(String expression) {
+        try {
+            cronParser.parse(expression).validate();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid cron expression: " + expression);
+        }
+    }
+}
