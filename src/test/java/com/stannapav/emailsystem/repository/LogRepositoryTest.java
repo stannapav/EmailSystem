1fb1917917c01d6f753ae8d01d37253fbd15ceac
stannapav <stannapav@gmail.com>
2026-01-22 16:47:14 +0200

    removed unused import

--- src/test/java/com/stannapav/emailsystem/repository/LogRepositoryTest.java
+++ src/test/java/com/stannapav/emailsystem/repository/LogRepositoryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @DataJpaTest
