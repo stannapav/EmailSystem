1cfd3e421bfbf5fa0f4ae228f8fe9d4ff05a7706
stannapav <stannapav@gmail.com>
2026-03-12 22:12:45 +0200

    Removed not needed repository annotation

--- src/main/java/com/stannapav/emailsystem/db/repositories/CronRepository.java
+++ src/main/java/com/stannapav/emailsystem/db/repositories/CronRepository.java
@@ -2,11 +2,9 @@ package com.stannapav.emailsystem.db.repositories;
 
 import com.stannapav.emailsystem.db.entities.CronJob;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface CronRepository extends JpaRepository<CronJob, Integer> {
 
     Optional<CronJob> findByExpression(String expression);
--- src/main/java/com/stannapav/emailsystem/db/repositories/UserRepository.java
+++ src/main/java/com/stannapav/emailsystem/db/repositories/UserRepository.java
@@ -4,11 +4,9 @@ import com.stannapav.emailsystem.db.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Page<User> findAllByOrderByCreatedOnDesc(Pageable pageable);
