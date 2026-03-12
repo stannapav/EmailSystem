610fc1679a18d31f576a6da9eb6879526bb183f8
stannapav <stannapav@gmail.com>
2026-03-12 22:13:17 +0200

    Removed not needed repository annotation and added SQL query for better data handling

--- src/main/java/com/stannapav/emailsystem/db/repositories/LogRepository.java
+++ src/main/java/com/stannapav/emailsystem/db/repositories/LogRepository.java
@@ -1,9 +1,27 @@
 package com.stannapav.emailsystem.db.repositories;
 
+import com.stannapav.emailsystem.db.dtos.UserStatProjection;
 import com.stannapav.emailsystem.db.entities.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
 public interface LogRepository extends JpaRepository<Log, Integer> {
+
+    @Query("""
+        SELECT 
+            u.username as username,
+            u.email as email,
+            SUM(CASE WHEN l.type = 'REST' THEN 1 ELSE 0 END) as rest,
+            SUM(CASE WHEN l.type = 'CRON' THEN 1 ELSE 0 END) as cron,
+            MIN(l.createdOn) as first,
+            MAX(l.createdOn) as last
+        FROM Log l
+        JOIN l.user u
+        GROUP BY u.id, u.username, u.email
+        ORDER BY (SUM(CASE WHEN l.type = 'REST' THEN 1 ELSE 0 END) +
+                  SUM(CASE WHEN l.type = 'CRON' THEN 1 ELSE 0 END)) DESC
+    """)
+    Page<UserStatProjection> getUserStats(Pageable pageable);
 }
